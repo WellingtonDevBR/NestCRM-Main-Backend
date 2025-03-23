8e905eab5d1cb0164e9ba79838c6297d1f9e19cb
Wellington <wellington.sydney1@gmail.com>
2025-03-24 00:19:31 +1100

    console logs

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,11 @@ app.use(cors({
     credentials: true
 }));
 
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ➡️ ${req.method} ${req.url}`);
+    next();
+});
+
 
 app.get('/', (_req: Request, res: Response) => {
     res.send('✅ EC2 instance is running and healthy!');
