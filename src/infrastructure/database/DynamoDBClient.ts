1ee9836a270e53eb5f13382b25a9bfbb91b909a4
Wellington <wellington.sydney1@gmail.com>
2025-03-24 00:10:44 +1100

    database .env

--- src/infrastructure/database/DynamoDBClient.ts
+++ src/infrastructure/database/DynamoDBClient.ts
@@ -1,16 +1,15 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, GetCommand, PutCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
-import { ENV } from "../../env";
 import { loadSecrets } from "../../utils/loadSecrets";
 
 await loadSecrets();
 
 // Initialize DynamoDB Client
 const dynamoDB = new DynamoDBClient({
-    region: ENV.AWS_REGION,
+    region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: ENV.AWS_ACCESS_KEY_ID,
-        secretAccessKey: ENV.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
     },
 });
 
