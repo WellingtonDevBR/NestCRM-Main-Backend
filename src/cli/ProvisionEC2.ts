32773fc1a0ba2c3be33fb80460bf3aafacf91aae
Wellington <wellington.sydney1@gmail.com>
2025-03-27 01:10:02 +1100

    fix provision

--- src/cli/ProvisionEC2.ts
+++ src/cli/ProvisionEC2.ts
@@ -50,6 +50,10 @@ export class ProvisionEC2 {
                     git clone https://github.com/WellingtonDevBR/NestCRM-Dashboard-Backend.git
                     cd NestCRM-Dashboard-Backend
                     yarn install
+                    sudo npm install pm2 -g
+                    pm2 start yarn --interpreter bash --name my-server -- ts-node-dev src/server.ts
+                    pm2 save
+                    pm2 startup
                     nohup yarn dev > /home/ec2-user/app.log 2>&1 &
                 `).toString("base64"),
             });
